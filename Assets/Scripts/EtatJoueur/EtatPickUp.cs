248d26da81073c63b91312c91453b2d075e7743f
Malak Fora <2067247@bdeb.qc.ca>
2024-03-31 12:40:04 -0400

    etats

--- Assets/Scripts/EtatJoueur/EtatPickUp.cs
+++ Assets/Scripts/EtatJoueur/EtatPickUp.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EtatPickUp : MonoBehaviour
+public class EtatPickUp : EtatJoueur
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+   
